a93a73f6dd03130031dec0d4e2a82786eebb5210
nethmi0121 <IT23391536@my.sliit.lk>
2025-10-17 14:25:49 +0530

    Remove migration 0001_mighty_silver_centurion.sql

--- backend/src/db/migrations/0001_mighty_silver_centurion.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX "waste_items_category_id_idx" ON "waste_items" USING btree ("category_id");--> statement-breakpoint
-CREATE INDEX "waste_logs_user_id_idx" ON "waste_logs" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "waste_logs_created_at_idx" ON "waste_logs" USING btree ("created_at");
